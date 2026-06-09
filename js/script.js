b69c2e1f548275abc5809100e00fd9f50e194177
tsholofelolelaka-cmd <tsholofelo.lelaka@techbridlefoundation.org>
2026-06-09 22:53:08 +0200

    Create script.js for dark mode

---
+++ js/script.js
@@ -0,0 +1,15 @@
+document.addEventListener('DOMContentLoaded', () => {
+  const toggleBtn = document.getElementById('theme-toggle');
+  // Toggle theme on click
+  toggleBtn.addEventListener('click', () => {
+    document.body.classList.toggle('dark-mode');
+    
+    if (document.body.classList.contains('dark-mode')) {
+      localStorage.setItem('theme', 'dark');
+      
+    } else {
+      localStorage.setItem('theme', 'light');
+      
+    }
+  });
+});
